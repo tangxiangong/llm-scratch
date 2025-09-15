e292d0278ec0cbd3c40578c74eb7c0770f76a4bb
tangxiangong <tangxiangong@gmail.com>
2025-09-15 11:28:06 +0800

    Add GPT-2 tokenizer and data loading utilities
    The changes include implementing a tokenizer for GPT-2 with special
    token support and a helper to load training data with better error
    handling.

--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,9 +1,55 @@
+use anyhow::Result;
+use std::ops::Deref;
+use tokenizers::{AddedToken, Tokenizer};
+
+/// GPT-2 tokenizer
+pub struct GPTTokenizer {
+    tokenizer: Tokenizer,
+    special_tokens: Vec<AddedToken>,
+}
+
+impl Deref for GPTTokenizer {
+    type Target = Tokenizer;
+
+    fn deref(&self) -> &Self::Target {
+        &self.tokenizer
+    }
+}
+
+impl GPTTokenizer {
+    pub fn new() -> Result<Self> {
+        let tokenizer =
+            Tokenizer::from_pretrained("gpt2", None).map_err(|e| anyhow::anyhow!("{}", e))?;
+        Ok(Self {
+            tokenizer,
+            special_tokens: vec![],
+        })
+    }
+
+    /// Add special tokens to the tokenizer.
+    pub fn add_special_tokens(mut self, special_tokens: &[&str]) -> Self {
+        let tokens = special_tokens.iter().map(|&s| AddedToken::from(s, true));
+        self.special_tokens.extend(tokens);
+        self.tokenizer.add_special_tokens(&self.special_tokens);
+        self
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn test_reader() {
-        let raw_text = std::fs::read_to_string("./data/the-verdict.txt").unwrap();
-        println!("Total number of character: {}", raw_text.len());
-        println!("{:?}", &raw_text[..100]);
+    fn test_tokenizer() {
+        let tokenizer = GPTTokenizer::new()
+            .unwrap()
+            .add_special_tokens(&["<|endoftext|>"]);
+        let tokens = tokenizer
+            .encode(
+                "Hello, do you like tea? <|endoftext|> In the sunlit terraces of someunkownPlace.",
+                true,
+            )
+            .unwrap();
+        println!("{:?}", tokens.get_ids());
     }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use anyhow::Context;
 use candle_core::Device;
 
 pub fn select_backend() -> Device {
@@ -7,5 +8,23 @@ pub fn select_backend() -> Device {
         device
     } else {
         Device::Cpu
+    }
+}
+
+pub fn load_data() -> anyhow::Result<String> {
+    let raw_text = std::fs::read_to_string("./data/the-verdict.txt")
+        .with_context(|| r#"Could not read data file "./data/the-verdict.txt""#)?;
+    Ok(raw_text)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load_data() {
+        let raw_text = load_data().unwrap();
+        println!("Total number of character: {}", raw_text.len());
+        println!("{:?}", &raw_text[..100]);
     }
 }
